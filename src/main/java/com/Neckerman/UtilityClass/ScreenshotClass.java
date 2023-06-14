3a3f532ad1d675f394b15ab2e359b0bc0139fd0e
Priyanka Lanjekar <Priyanka Lanjekar@WA000034>
2023-06-14 11:38:38 +0530

    space added

--- src/main/java/com/Neckerman/UtilityClass/ScreenshotClass.java
+++ src/main/java/com/Neckerman/UtilityClass/ScreenshotClass.java
@@ -20,6 +20,7 @@ public class ScreenshotClass {
 			DateFormat d1 = new SimpleDateFormat("yy-mm-dd & HH-mm-ss");//MM-dd-yy-->correct format
 			String date = d1.format(d);
 			
+			
 			TakesScreenshot ts= (TakesScreenshot)driver;
 			File source = ts.getScreenshotAs(OutputType.FILE);
 			File destination = new File("./Neckermann_ScreenshotFolder\\"+date+name+".jpg");	
